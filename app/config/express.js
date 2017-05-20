e3d80b8436281e566c21b1c846bba09bde56b4c8
astider@home <astider@gmail.com>
2017-05-20 23:34:03 +0700

    update express.js

--- app/config/express.js
+++ app/config/express.js
@@ -28,73 +28,41 @@ module.exports = function(app, express) {
 
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
-
-  let allowedHeader = ["http://localhost:3000", "https://dsmbot.herokuapp.com", "https://messengerchatbot-f6775.firebaseapp.com"]
-  app.use(function(req, res, next) {
-
-    var origin = req.get('origin');
-    //console.log(req.session);
-    if (origin) {
-      console.log(`origin: ${origin}, ....... ${JSON.stringify(origin)}`);
-     if (allowedHeader.indexOf(origin) > -1){
-      res.header("Access-Control-Allow-Origin", "*")
-     }
-     else{
-     return res.status(403).end();
-     }
-    }
-
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
-
-    console.log(`res.header = ${res.header}`);
-    console.log(`req.method = ${req.method}`);
-    if ('OPTIONS' == req.method) {
-     return res.status(200).end();
-    }
-
-    next();
-
-  })
+  //
+  // let allowedHeader = ["http://localhost:3000", "https://dsmbot.herokuapp.com", "https://messengerchatbot-f6775.firebaseapp.com"]
+  // app.use(function(req, res, next) {
+  //
+  //   var origin = req.get('origin');
+  //   //console.log(req.session);
+  //   if (origin) {
+  //     console.log(`origin: ${origin}, ....... ${JSON.stringify(origin)}`);
+  //    if (allowedHeader.indexOf(origin) > -1){
+  //     res.header("Access-Control-Allow-Origin", "*")
+  //    }
+  //    else{
+  //    return res.status(403).end();
+  //    }
+  //   }
+  //
+  //   res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+  //   res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
+  //
+  //   console.log(`res.header = ${res.header}`);
+  //   console.log(`req.method = ${req.method}`);
+  //   if ('OPTIONS' == req.method) {
+  //    return res.status(200).end();
+  //   }
+  //
+  //   next();
+  //
+  // })
 
   //app.use(allowCrossDomain)
   app.use(express.static('public'))
 
 
 
-  app.get("/", quizControl.getControlInterface)
-  app.get("/info", index.getIndexPage)
-  app.get("/policy", index.getPolicyPage)
-  app.get("/result/", quizResult.getResult)
-  //app.get("/addquiz", quizInput.addQuiz)addQuizV2
-  app.get("/addquiz", quizInput.addQuizV2)
-
-
-  app.get("/getAllStatus", quizControl.getAllStatus)
-  app.get("/justStartTheQuiz", quizControl.startQuiz)
-  app.get("/activateQ", quizControl.activateQ)
-  app.get("/closeAnswerTime", quizControl.endAnswerTime)
-  app.get("/endQuizNow", quizControl.endQuizNow)
-  app.get("/changeReadyToStart", quizControl.changeReadyToStart)
-  app.get("/changeEnterStatus", quizControl.changeEnterStatus)
-  app.get("/controlRoom", quizControl.getControlInterface)
-
-  // API for front-end
-  app.get("/getAllSingleUsers", quizAPIs.getAllSingleUsers)
-  app.get("/getCorrectUsers", quizAPIs.getCorrectUsersInfo)
-  app.get("/getAllUsersInfo", quizAPIs.getAllUsersInfo)
-  app.get("/getAllParticipantsInfo", quizAPIs.getAllParticipantsInfo)
-  app.get("/getAllQuestions", quizAPIs.getAllQuestions)
-  app.get("/getParticipantsScoreObject", quizAPIs.getParticipantsScoreObject)
-  app.get("/getParticipantsScore", quizAPIs.getParticipantsScore)
-
-  app.get("/v2/getRecentVotes", quizAPIsV2.getRecentVotes)
-  app.get("/v2/getVoteResult", quizAPIsV2.getVoteResult)
-  app.get("/v2/getQuestion", quizAPIsV2.getQuestion)
-  app.get("/v2/getAllUsersInfo", quizAPIsV2.getAllUsersInfo)
-
-  //app.post("/processQuizForm", jsonParser, urlencodedParser, quizInput.processForm)
-  app.post("/processQuizForm", jsonParser, urlencodedParser, quizInput.processFormV2)
+  app.get("/", function(req, res) { res.send('yay') })
 
   app.get("/*", (req, res) => { res.render("404") } )
 
