a6473d033f47027e5ecb57100b89a4ebf1ade1fc
astider@home <astider@gmail.com>
2017-06-27 08:09:00 +0700

    update status: now update supr stats

--- server.js
+++ server.js
@@ -297,7 +297,23 @@ botmaster.on('update', (bot, update) => {
 
     }
     else if(command === "UPDATE_STATUS") {
-      bot.sendTextCascadeTo([`STR 10, DEX 1, VIT 5, INT 0`, `You don't have status point to be used.`], update.sender.id)
+      //bot.sendTextCascadeTo([`STR 10, DEX 1, VIT 5, INT 0`, `You don't have status point to be used.`], update.sender.id)
+      fetch('https://lbry.suprnova.cc/index.php?page=api&action=getuserbalance&api_key=61ef9d9818cc2932be1071c8a53a50a7853830ba62b8bd4486a76c27324fe029&id=999317')
+      .then(res => { return res.json() })
+      .then(jsonData => {
+
+        let lbcData = jsonData.getuserbalance.data
+        let texts = [ 'orphan: ' + lbcData.orphaned,
+                      'unconf: ' + lbcData.unconfirmed,
+                      'confirmed: ' + lbcData.confirmed,
+                      'un+conf:' + parseFloat(lbcData.unconfirmed + lbcData.confirmed)
+                    ]
+          messengerBot.sendTextCascadeTo(texts, '1371226459627784')
+      })
+      .catch(error => {
+        console.log('error sending supr: ' + error);
+      })
+      
     }
 
   }
@@ -307,23 +323,6 @@ botmaster.on('update', (bot, update) => {
 function rounder(floatNumber, point) {
   return parseInt( floatNumber * Math.pow(10, point) ) / Math.pow(10, point)
 }
-
-
-fetch('https://lbry.suprnova.cc/index.php?page=api&action=getuserbalance&api_key=61ef9d9818cc2932be1071c8a53a50a7853830ba62b8bd4486a76c27324fe029&id=999317')
-.then(res => { return res.json() })
-.then(jsonData => {
-
-  let lbcData = jsonData.getuserbalance.data
-  let texts = [ 'orphan: ' + lbcData.orphaned,
-                'unconf: ' + lbcData.unconfirmed,
-                'confirmed: ' + lbcData.confirmed,
-                'un+conf:' + parseFloat(lbcData.unconfirmed + lbcData.confirmed)
-              ]
-    messengerBot.sendTextCascadeTo(texts, '1371226459627784')
-})
-.catch(error => {
-  console.log('error sending supr: ' + error);
-})
 
 
 let nodeSchedule = require('node-schedule');
